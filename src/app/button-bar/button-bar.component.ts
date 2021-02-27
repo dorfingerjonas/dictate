0d35c39daccfdc4decec66a0fd96afa1efe0e656
dorfingerjonas <jonas.dorfinger@gmx.at>
2021-02-28 00:54:26 +0100

    bind attribute and event handler to buttons

--- src/app/button-bar/button-bar.component.ts
+++ src/app/button-bar/button-bar.component.ts
@@ -1,4 +1,4 @@
-import {Component, Input, OnInit} from '@angular/core';
+import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
 import {SpeechRecognitionService} from '../speech-recognition.service';
 
 @Component({
@@ -10,6 +10,8 @@ export class ButtonBarComponent implements OnInit {
 
   @Input() content = '';
   @Input() speechRecoService: SpeechRecognitionService;
+  @Output() isContinued = new EventEmitter<boolean>();
+  recoOnceStarted = false;
 
   constructor() {
   }
@@ -17,10 +19,26 @@ export class ButtonBarComponent implements OnInit {
   ngOnInit(): void {
   }
 
-  startReco(): void {
+  start(): void {
     if (!this.speechRecoService.recoStarted) {
+      this.recoOnceStarted = true;
       this.speechRecoService.start();
     }
   }
 
+  stop(): void {
+    this.speechRecoService.stop();
+  }
+
+  reset(): void {
+    this.content = '';
+    this.recoOnceStarted = false;
+    this.speechRecoService.stop();
+    this.isContinued.emit(false);
+  }
+
+  continue(): void {
+    this.isContinued.emit(true);
+    this.speechRecoService.start();
+  }
 }
