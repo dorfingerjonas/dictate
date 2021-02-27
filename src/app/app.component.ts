c66d8db6c01d65cd44f656b08239a9530f6f3297
dorfingerjonas <jonas.dorfinger@gmx.at>
2021-02-28 00:00:24 +0100

    implement ngOnInit() and import speechRecoService

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {SpeechRecognitionService} from './speech-recognition.service';
+import {ConsoleLogger} from '@angular/compiler-cli/ngcc';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'dictate';
+  content = '';
+
+  constructor(public speechReco: SpeechRecognitionService) {
+  }
+
+  ngOnInit(): void {
+    this.speechReco.init();
+
+    this.speechReco.interimResult.subscribe(result => {
+      this.content = result;
+    });
+
+    this.speechReco.speechEnded.subscribe(() => {
+
+    });
+  }
 }
